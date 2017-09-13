e5e6b39b097cd63bd11d1b00e8805c253dfa84bb
slandau3 <stevenlandau32@gmail.com>
2017-09-12 20:38:44 -0400

    added sql db initialization file

---
+++ sql/db_craetion.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "role";
+DROP TABLE IF EXISTS role_permission;
+DROP TABLE IF EXISTS address;
+DROP TABLE IF EXISTS federal_laws;
+DROP TABLE IF EXISTS state_laws;
+DROP TABLE IF EXISTS discussions;
+DROP TABLE IF EXISTS thread;
+DROP TABLE IF EXISTS forum_post;
+
+CREATE TABLE "user" (
+  id SERIAL PRIMARY KEY,
+  first_name text NOT NULL,
+  last_name text NOT NULL,
+  password text NULL, --Can be null if social login is used
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE "role" (
+  id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE role_permission (
+  id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE address (
+  id SERIAL PRIMARY KEY,
+  street_1 text,
+  street_2 text,
+  city text NOT NULL,
+  state text NOT NULL,
+  postal_code text,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE federal_laws (
+
+);
+
+CREATE TABLE state_laws (
+
+);
+
+CREATE TABLE discussions (
+
+);
+
+CREATE TABLE thread (
+
+);
+
+CREATE TABLE forum_post (
+
+);
+
