ea3b8f22a951d9c5f188614b883939de6ffb3586
Steven Landau <slandau@juji-inc.com>
2017-10-19 09:40:32 -0400

    removed unneeded javacsript

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 app = Flask(__name__)
 app.secret_key = "secret key"
 
 @app.route('/')
 def hello_word():
     return render_template('home.html', user="steven")
+
 
 
 if __name__ == '__main__':
