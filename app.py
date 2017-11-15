7ab67681e2ba5006ccf8e10a869d00b87b986082
slandau3 <slandau@juji-inc.com>
2017-11-15 16:52:49 -0500

    did some stuff

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+"""
+TODO
+"""
+
+import json
 import business_logic.account.account as account
 import business_logic.sql.db_adapter as db
-import json
 
 # initialize flask
 from flask import Flask, session, render_template, \
@@ -14,11 +18,12 @@ app.secret_key = "secret key"
 def home():
     return render_template('index.html')
 
-@app.route('/login/', methods = ['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
     """
     TODO
     """
+    print("help")
     if request.method == 'POST':
         # Attempt to obtain the username and password from the form
         username = request.form.get('username', False)
@@ -36,11 +41,13 @@ def login():
     elif request.method == 'GET':
         return render_template('login.html')
     else:
-        raise NotImplemented()
+        raise NotImplementedError()
 
-
-@app.route('/createAccount/', methods = ['GET', 'POST'])
+@app.route('/createAccount/', methods=['GET', 'POST'])
 def create_account():
+    """
+    TODO
+    """
     if request.method == 'GET':
         return render_template('createAccount.html', author="Tory", name="steven")
     elif request.method == 'POST':
@@ -67,9 +74,9 @@ def create_account():
         else:
             return Response(json.dumps(create_account_resp), mimetype='application/json; charset=utf-8')
     else:
-        raise NotImplemented()
-		
-@app.route('/forum/discussions/', methods = ['GET'])
+        raise NotImplementedError()
+
+@app.route('/forum/discussions/', methods=['GET'])
 def load_forum_discussions():
     if request.method == 'GET':
         discussions = db.getDiscussions()
